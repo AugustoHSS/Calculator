0d6e5a82018ca822c692a0ee8f94a0ec463ad554
augusto hansel schaefer dos santos <augustohansel@hotmail.com>
2026-06-15 22:11:11 -0300

    add the erase all button

--- javascript.js
+++ javascript.js
@@ -1,70 +1,76 @@
-function doTheOperation(firstNumber, secondNumber, operator) {
-  const a = Number(firstNumber);
-  const b = Number(secondNumber);
+function createCalculator() {
 
-  const operations = {
-    '+': (x, y) => x + y,
-    '-': (x, y) => x - y,
-    '*': (x, y) => x * y,
-    '/': (x, y) => x / y,
-  };
-
-  const result = operations[operator](a, b);
-
-  updateDisplay(result);
-  operator = "";
-  firstNumber = result.toString();
-  secondNumber = "";
-
-}
-
-function updateDisplay(value){
-    const display = document.getElementsByClassName("calculator-display")[0];
-    display.textContent = value;
-}
-
-
-let displayValue = ""
-let firstNumber = "";
-let secondNumber = "";
-let operator = "";
-function calculator(clickedButton){
-
-    if(/^[0-9]$/.test(clickedButton)){
-        if(operator === ""){
-            firstNumber += clickedButton;
-            updateDisplay(firstNumber)
-        }else{ 
-            secondNumber += clickedButton;
-            updateDisplay(secondNumber)
-
-        }
-    }
-
-    if(clickedButton === "+" ||
-       clickedButton === "-" ||
-       clickedButton === "/" ||
-       clickedButton === "*"
-    ){
-        operator = clickedButton;
-        if(secondNumber !== ""){
-            doTheOperation(firstNumber, secondNumber ,operator);
-        }
-    }
-
-    if(clickedButton === "=" && firstNumber !== "" && secondNumber !== ""){
-        doTheOperation(firstNumber, secondNumber ,operator)
-    }
+    let displayValue = ""
+    let firstNumber = "";
+    let secondNumber = "";
+    let operator = "";
     
-}
+    const operations = {
+        '+': (x, y) => x + y,
+        '-': (x, y) => x - y,
+        '*': (x, y) => x * y,
+        '/': (x, y) => x / y,
+    };
 
-createButtonsEventListenerButtons()
+    function doTheOperation() {
+    const a = Number(firstNumber);
+    const b = Number(secondNumber);
 
-function createButtonsEventListenerButtons(){
-        const buttons = document.querySelectorAll("button");
-        buttons.forEach(button => {
+    const result = operations[operator](a, b);
+
+    updateDisplay(result);
+
+    firstNumber = result.toString();
+    secondNumber = "";
+
+    }
+
+    function updateDisplay(value){
+        const display = document.getElementsByClassName("calculator-display")[0];
+        display.textContent = value;
+    }
+
+    function calculator(clickedButton){
+
+        if(/^[0-9]$/.test(clickedButton)){
+            if(operator === ""){
+                firstNumber += clickedButton;
+                updateDisplay(firstNumber)
+            }else{ 
+                secondNumber += clickedButton;
+                updateDisplay(secondNumber)
+
+            }
+        }
+
+        if(clickedButton === "AC"){
+            resetCalculator()
+        } 
+
+        if(operations[clickedButton]){
+            operator = clickedButton;
+            if(secondNumber !== ""){
+                doTheOperation(firstNumber, secondNumber ,operator);
+            }
+        }
+
+        if(clickedButton === "=" && firstNumber !== "" && secondNumber !== ""){
+            doTheOperation(firstNumber, secondNumber ,operator)
+        }
+        
+    }
+
+    function resetCalculator(){
+        displayValue = "0"
+        firstNumber = "";
+        secondNumber = "";
+        operator = "";
+        updateDisplay(displayValue)
+    }
+    document.querySelectorAll("button").forEach(button => {
         button.addEventListener("click", () => {
             calculator(button.textContent)
-       });
+        });
     });
 }
+createCalculator();
